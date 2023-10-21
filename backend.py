1b6109c3189fa62c9fad2ec9427b579f474b4052
kenny <dadzieato317@gmail.com>
2023-10-21 20:30:56 +0000

    ploting and adding images to the project

---
+++ backend.py
@@ -0,0 +1,17 @@
+import requests
+APIkey ='08f8b2ae06822219e792c4e0c6c6f145'
+def get_data(place,days):
+
+     url =f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={APIkey}"
+     response =requests.get(url)
+     context = response.json()
+     filtered_context = context['list']
+     nr_values = 8 * days
+     filtered_context = filtered_context[:nr_values]
+     return filtered_context
+
+
+
+
+if __name__ == '__main__':
+     print(get_data(place='Accra',days=2,option='Sky'))
--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import streamlit as st
-
+import plotly.express as px
+import pandas as pd
+from backend import get_data
 st.set_page_config(layout='wide')
 
 st.title('Weather forcast for the next days')
-place=st.text_input(label='place:',key='place')
+place= st.text_input('place:')
 st.text('Forcast Days')
 days = st.slider('Select a value', min_value=0, max_value=5, value=0, step=1,help='select the number of forcasted days')
 
 
 
-Dropdown = st.selectbox(label='Select data to view',options=['Temperature','sky'])
+option = st.selectbox(label='Select data to view',options=['Temperature','Sky'])
 
-st.header(f'{Dropdown}for the next {days} in {place}')
+st.header(f'{option}for the next {days} days in {place}')
+
+
+if place:
+    filtered_context = get_data(place, days)
+    if option == 'Temperature':
+        temperatures = [dict['main']['temp'] for dict in filtered_context]
+        dates = [dict['dt_txt'] for dict in filtered_context]
+        figure = px.line(x=dates, y=temperatures,labels ={'x':'Date','y':'Temperature (C)'})
+        st.plotly_chart(figure)
+    if option == 'Sky':
+        images ={
+            'Clear':'images/clear.png',
+            'Clouds':'images/cloud.png',
+            'Rain': 'images/rain.png',
+            'Snow': 'images/snow.png'
+        }
+        sky_conditons = [dic['weather'][0]['main'] for dic in filtered_context]
+        print(sky_conditons)
+        image_paths =[images[condition] for condition in sky_conditons]
+        st.image(image_paths,width=115)
